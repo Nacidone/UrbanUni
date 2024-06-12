5eff8c8dd14d881dcdfe60650772244122416a5f
Nacidone <60882337+Nacidone@users.noreply.github.com>
2024-06-12 06:55:11 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+start_list = 0
+while start_list < len(my_list):
+    if my_list[start_list] < 0:
+        break
+    elif my_list[start_list] > 0:
+        print(my_list[start_list])
+    start_list = start_list + 1
